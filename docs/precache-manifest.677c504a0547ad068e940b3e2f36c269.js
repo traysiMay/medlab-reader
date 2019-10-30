747876a099a6031ab02a425d9deee0d9e5340fec
traysimay <traysimay@rumormill.fyi>
2019-10-30 09:42:33 -0700

    idunno

---
+++ docs/precache-manifest.677c504a0547ad068e940b3e2f36c269.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20405972ef608193d8c9c270b1f2066a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02ae93a2def786805c23",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e0c7394569391987b9eb",
+    "url": "/static/js/2.530260fe.chunk.js"
+  },
+  {
+    "revision": "02ae93a2def786805c23",
+    "url": "/static/js/main.f829d519.chunk.js"
+  },
+  {
+    "revision": "0d84825361f0df04df78",
+    "url": "/static/js/runtime~main.470bddd4.js"
+  }
+]);
